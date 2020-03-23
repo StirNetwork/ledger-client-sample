a52bb9d7d1b3f7f8d78ec3008980a1917fc80a3f
Dragon-taro <dragon.taro.lioil@gmail.com>
2020-03-23 17:07:10 +0900

    トランザクション作成に挑戦

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { CosmosSDK, AccAddress, ValAddress } from "cosmos-client";
+import { staking, DelegateRequest } from "cosmos-client/x/staking";
+
+const sdk = new CosmosSDK(
+  "https://cosmos-mainnet-s-001.k80s.net/",
+  "cosmoshub-3"
+);
+const DELEGATOR_ADDRESS = "cosmos1z52hq26g5n3la3m8sucrpessx33dgedw3cgvn0";
 
 function App() {
+  const handleDelegation = async () => {
+    const delegationReq: DelegateRequest = {
+      base_req: {
+        from: DELEGATOR_ADDRESS,
+        memo: "",
+        chain_id: sdk.chainID,
+        account_number: "0",
+        sequence: "1",
+        fees: [],
+        gas: "",
+        gas_adjustment: "",
+        gas_prices: [],
+        simulate: false
+      },
+      delegator_address: AccAddress.fromBech32(DELEGATOR_ADDRESS),
+      validator_address: ValAddress.fromBech32(
+        "cosmosvaloper1qdxmyqkvt8jsxpn5pp45a38ngs36mn2604cqk9"
+      ),
+      amount: {
+        denom: "uatom",
+        amount: "0.0001"
+      }
+    };
+    const unsinedTxs = await staking.postDelegation(
+      sdk,
+      AccAddress.fromBech32(DELEGATOR_ADDRESS),
+      delegationReq
+    );
+
+    console.log(unsinedTxs);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello
+      <button onClick={handleDelegation}>テスト</button>
     </div>
   );
 }
